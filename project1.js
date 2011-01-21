dd971a67df1271c7532516ccb98b0bed0aeb0cf9
Chris <cjw7488@gmail.com>
2011-01-21 11:19:35 -0600

    all validation DHTML using js file updated

--- project1.js
+++ project1.js
@@ -61,14 +61,106 @@ function validate () {
 		}		
 	}
 	
-/*	var addressErrorField = document.createElement("span");
-	addressErrorField.className = "errorField";
-	addressErrorField.appendChild(document.createTextNode("Your address is required."));
-	document.getElementById("inputAddressContainer").appendChild(addressErrorField);
+	// Address validation
 	
-	var cityErrorField = document.createElement("span");
-	cityErrorField.className = "errorField";
-	cityErrorField.appendChild(document.createTextNode("Your city is required."));
-	document.getElementById("inputCityContainer").appendChild(cityErrorField);
-*/	
+	if (document.getElementById("Address").value == "") {
+		if (!document.getElementById("addressErrorField")) {
+			var addressErrorField = document.createElement("span");
+			addressErrorField.setAttribute("id", "addressErrorField");
+			addressErrorField.className = "errorField";
+			addressErrorField.appendChild(document.createTextNode("Your address is required."));
+			document.getElementById("inputAddressContainer").appendChild(addressErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("addressErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+	
+	// City validation
+	
+	if (document.getElementById("City").value == "") {
+		if (!document.getElementById("cityErrorField")) {
+			var cityErrorField = document.createElement("span");
+			cityErrorField.setAttribute("id", "cityErrorField");
+			cityErrorField.className = "errorField";
+			cityErrorField.appendChild(document.createTextNode("Your city is required."));
+			document.getElementById("inputCityContainer").appendChild(cityErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("cityErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+	
+	// State validation
+	
+	if (document.getElementById("State").value == "None") {
+		if (!document.getElementById("stateErrorField")) {
+			var stateErrorField = document.createElement("span");
+			stateErrorField.setAttribute("id", "stateErrorField");
+			stateErrorField.className = "errorField";
+			stateErrorField.appendChild(document.createTextNode("Your state is required."));
+			document.getElementById("inputStateContainer").appendChild(stateErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("stateErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+	
+	// Zip validation
+	
+	if (document.getElementById("Zip").value == "") {
+		if (!document.getElementById("zipErrorField")) {
+			var zipErrorField = document.createElement("span");
+			zipErrorField.setAttribute("id", "zipErrorField");
+			zipErrorField.className = "errorField";
+			zipErrorField.appendChild(document.createTextNode("Your zip code is required."));
+			document.getElementById("inputZipContainer").appendChild(zipErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("zipErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+	
+	// Gender validation
+	
+	if ((!document.getElementById("male").checked) && (!document.getElementById("female").checked)) {
+		if (!document.getElementById("genderErrorField")) {
+			var genderErrorField = document.createElement("span");
+			genderErrorField.setAttribute("id", "genderErrorField");
+			genderErrorField.className = "errorField";
+			genderErrorField.appendChild(document.createTextNode("Your gender is required."));
+			document.getElementById("inputGenderContainer").appendChild(genderErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("genderErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+	
+	// Course validation
+	
+	if ((!document.getElementById("php").checked) && (!document.getElementById("java").checked) && (!document.getElementById("rails").checked)) {
+		if (!document.getElementById("classErrorField")) {
+			var classErrorField = document.createElement("span");
+			classErrorField.setAttribute("id", "classErrorField");
+			classErrorField.className = "errorField";
+			classErrorField.appendChild(document.createTextNode("A course is required."));
+			document.getElementById("inputCourseContainer").appendChild(classErrorField);
+		}
+	} else {
+		var removeError = document.getElementById("classErrorField");
+		if (removeError) {
+			removeError.parentNode.removeChild(removeError);
+		}		
+	}
+
 } 
